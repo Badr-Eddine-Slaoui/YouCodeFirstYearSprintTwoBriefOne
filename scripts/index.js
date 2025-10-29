970b68283fda009538c8f77db69cfd4d3111663f
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2025-10-29 01:05:39 +0100

    Add Mind Game Difficulties Page HTML Structure

--- scripts/index.js
+++ scripts/index.js
@@ -2,9 +2,9 @@ let mindGameButton = document.getElementById("mind-game");
 let typingGameButton = document.getElementById("typing-game");
 
 mindGameButton.addEventListener("click", function () {
-    window.location.href = "./mind-game.html";
+    window.location.href = "./pages/mind-game-difficulty.html";
 });
 
 typingGameButton.addEventListener("click", function () {
-    window.location.href = "./typing-game.html";
+    window.location.href = "./pages/typing-game-cooldowns.html";
 });
